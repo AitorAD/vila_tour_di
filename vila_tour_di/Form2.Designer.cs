ad36302676a351e999b18b726e10e536d96c19b9
Javier Otero <javii.oteero@gmail.com>
2024-10-17 13:02:44 +0200

    Admin Form created, unified controls names

--- vila_tour_di/Form2.Designer.cs
+++ vila_tour_di/Form2.Designer.cs
@@ -25,12 +25,13 @@ namespace vila_tour_di {
         /// </summary>
         private void InitializeComponent() {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnAdminRoutes = new System.Windows.Forms.Button();
+            this.btnAdminRecipes = new System.Windows.Forms.Button();
+            this.btnAdminTraditions = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.btnAdminPlaces = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -38,104 +39,118 @@ namespace vila_tour_di {
             // 
             this.tableLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.7732F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 74.22681F));
-            this.tableLayoutPanel1.Controls.Add(this.button4, 0, 4);
-            this.tableLayoutPanel1.Controls.Add(this.button3, 0, 3);
-            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 2);
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel1.Controls.Add(this.btnExit, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.btnAdminRoutes, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.btnAdminRecipes, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnAdminTraditions, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblWelcome, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnAdminPlaces, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(10);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 5;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 561);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // button4
+            // btnExit
             // 
-            this.button4.AutoSize = true;
-            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button4.Location = new System.Drawing.Point(10, 458);
-            this.button4.Margin = new System.Windows.Forms.Padding(10);
-            this.button4.Name = "button4";
-            this.button4.Padding = new System.Windows.Forms.Padding(25);
-            this.button4.Size = new System.Drawing.Size(182, 93);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "Salir";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.btnExit.AutoSize = true;
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(10, 466);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(10);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(176, 85);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Salir";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
-            // button3
+            // btnAdminRoutes
             // 
-            this.button3.AutoSize = true;
-            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button3.Location = new System.Drawing.Point(10, 346);
-            this.button3.Margin = new System.Windows.Forms.Padding(10);
-            this.button3.Name = "button3";
-            this.button3.Padding = new System.Windows.Forms.Padding(25);
-            this.button3.Size = new System.Drawing.Size(182, 92);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Adminisrar Recetas";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnAdminRoutes.AutoSize = true;
+            this.btnAdminRoutes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAdminRoutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdminRoutes.Location = new System.Drawing.Point(10, 366);
+            this.btnAdminRoutes.Margin = new System.Windows.Forms.Padding(10);
+            this.btnAdminRoutes.Name = "btnAdminRoutes";
+            this.btnAdminRoutes.Size = new System.Drawing.Size(176, 80);
+            this.btnAdminRoutes.TabIndex = 5;
+            this.btnAdminRoutes.Text = "Rutas";
+            this.btnAdminRoutes.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnAdminRecipes
             // 
-            this.button2.AutoSize = true;
-            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button2.Location = new System.Drawing.Point(10, 234);
-            this.button2.Margin = new System.Windows.Forms.Padding(10);
-            this.button2.Name = "button2";
-            this.button2.Padding = new System.Windows.Forms.Padding(25);
-            this.button2.Size = new System.Drawing.Size(182, 92);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Adminisrar Fiestas y Tradiciones";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnAdminRecipes.AutoSize = true;
+            this.btnAdminRecipes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAdminRecipes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdminRecipes.Location = new System.Drawing.Point(10, 266);
+            this.btnAdminRecipes.Margin = new System.Windows.Forms.Padding(10);
+            this.btnAdminRecipes.Name = "btnAdminRecipes";
+            this.btnAdminRecipes.Size = new System.Drawing.Size(176, 80);
+            this.btnAdminRecipes.TabIndex = 4;
+            this.btnAdminRecipes.Text = "Recetas";
+            this.btnAdminRecipes.UseVisualStyleBackColor = true;
+            // 
+            // btnAdminTraditions
+            // 
+            this.btnAdminTraditions.AutoSize = true;
+            this.btnAdminTraditions.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAdminTraditions.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdminTraditions.Location = new System.Drawing.Point(10, 166);
+            this.btnAdminTraditions.Margin = new System.Windows.Forms.Padding(10);
+            this.btnAdminTraditions.Name = "btnAdminTraditions";
+            this.btnAdminTraditions.Size = new System.Drawing.Size(176, 80);
+            this.btnAdminTraditions.TabIndex = 3;
+            this.btnAdminTraditions.Text = "Fiestas y Tradiciones";
+            this.btnAdminTraditions.UseVisualStyleBackColor = true;
+            this.btnAdminTraditions.Click += new System.EventHandler(this.btnAdminTraditions_Click);
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(205, 3);
+            this.panel1.Location = new System.Drawing.Point(199, 3);
             this.panel1.Name = "panel1";
-            this.tableLayoutPanel1.SetRowSpan(this.panel1, 5);
-            this.panel1.Size = new System.Drawing.Size(576, 555);
+            this.tableLayoutPanel1.SetRowSpan(this.panel1, 6);
+            this.panel1.Size = new System.Drawing.Size(582, 555);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // lblWelcome
             // 
-            this.label1.AutoSize = true;
-            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(196, 112);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Bienvenido XXX:";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblWelcome.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.Location = new System.Drawing.Point(3, 0);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(190, 56);
+            this.lblWelcome.TabIndex = 1;
+            this.lblWelcome.Text = "Bienvenido XXX:";
+            this.lblWelcome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // button1
+            // btnAdminPlaces
             // 
-            this.button1.AutoSize = true;
-            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.Location = new System.Drawing.Point(10, 122);
-            this.button1.Margin = new System.Windows.Forms.Padding(10);
-            this.button1.Name = "button1";
-            this.button1.Padding = new System.Windows.Forms.Padding(25);
-            this.button1.Size = new System.Drawing.Size(182, 92);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Adminisrar Lugares";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnAdminPlaces.AutoSize = true;
+            this.btnAdminPlaces.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAdminPlaces.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdminPlaces.Location = new System.Drawing.Point(10, 66);
+            this.btnAdminPlaces.Margin = new System.Windows.Forms.Padding(10);
+            this.btnAdminPlaces.Name = "btnAdminPlaces";
+            this.btnAdminPlaces.Size = new System.Drawing.Size(176, 80);
+            this.btnAdminPlaces.TabIndex = 2;
+            this.btnAdminPlaces.Text = "Lugares";
+            this.btnAdminPlaces.UseVisualStyleBackColor = true;
+            this.btnAdminPlaces.Click += new System.EventHandler(this.btnAdminPlaces_Click);
             // 
             // Form2
             // 
@@ -145,7 +160,6 @@ namespace vila_tour_di {
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form2";
             this.Text = "Redactor";
-            this.Load += new System.EventHandler(this.Form2_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
@@ -156,10 +170,11 @@ namespace vila_tour_di {
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Button btnAdminPlaces;
+        private System.Windows.Forms.Button btnAdminRoutes;
+        private System.Windows.Forms.Button btnAdminRecipes;
+        private System.Windows.Forms.Button btnAdminTraditions;
+        private System.Windows.Forms.Button btnExit;
     }
 }
---
+++ vila_tour_di/Form3.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace vila_tour_di
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnAdminPlace = new System.Windows.Forms.Button();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAdminRoutes = new System.Windows.Forms.Button();
+            this.btnAdminRecipes = new System.Windows.Forms.Button();
+            this.btnAdminTraditions = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnAdminUsers = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel1.Controls.Add(this.btnAdminUsers, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.btnExit, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.btnAdminPlace, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblWelcome, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnAdminRoutes, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.btnAdminRecipes, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnAdminTraditions, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.34483F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.94253F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.94253F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.94253F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.94253F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.94253F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.94253F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 561);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnAdminPlace
+            // 
+            this.btnAdminPlace.AutoSize = true;
+            this.btnAdminPlace.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAdminPlace.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdminPlace.Location = new System.Drawing.Point(10, 68);
+            this.btnAdminPlace.Margin = new System.Windows.Forms.Padding(10);
+            this.btnAdminPlace.Name = "btnAdminPlace";
+            this.btnAdminPlace.Size = new System.Drawing.Size(176, 63);
+            this.btnAdminPlace.TabIndex = 0;
+            this.btnAdminPlace.Text = "Lugares";
+            this.btnAdminPlace.UseVisualStyleBackColor = true;
+            this.btnAdminPlace.Click += new System.EventHandler(this.btnAdminPlace_Click);
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblWelcome.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.Location = new System.Drawing.Point(3, 0);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(190, 58);
+            this.lblWelcome.TabIndex = 0;
+            this.lblWelcome.Text = "Bienvenido, XXX";
+            this.lblWelcome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(199, 3);
+            this.panel1.Name = "panel1";
+            this.tableLayoutPanel1.SetRowSpan(this.panel1, 7);
+            this.panel1.Size = new System.Drawing.Size(582, 555);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnAdminRoutes
+            // 
+            this.btnAdminRoutes.AutoSize = true;
+            this.btnAdminRoutes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAdminRoutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdminRoutes.Location = new System.Drawing.Point(10, 317);
+            this.btnAdminRoutes.Margin = new System.Windows.Forms.Padding(10);
+            this.btnAdminRoutes.Name = "btnAdminRoutes";
+            this.btnAdminRoutes.Size = new System.Drawing.Size(176, 63);
+            this.btnAdminRoutes.TabIndex = 3;
+            this.btnAdminRoutes.Text = "Rutas";
+            this.btnAdminRoutes.UseVisualStyleBackColor = true;
+            this.btnAdminRoutes.Click += new System.EventHandler(this.btnAdminRoutes_Click);
+            // 
+            // btnAdminRecipes
+            // 
+            this.btnAdminRecipes.AutoSize = true;
+            this.btnAdminRecipes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAdminRecipes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdminRecipes.Location = new System.Drawing.Point(10, 234);
+            this.btnAdminRecipes.Margin = new System.Windows.Forms.Padding(10);
+            this.btnAdminRecipes.Name = "btnAdminRecipes";
+            this.btnAdminRecipes.Size = new System.Drawing.Size(176, 63);
+            this.btnAdminRecipes.TabIndex = 2;
+            this.btnAdminRecipes.Text = "Recetas";
+            this.btnAdminRecipes.UseVisualStyleBackColor = true;
+            this.btnAdminRecipes.Click += new System.EventHandler(this.btnAdminRecipes_Click);
+            // 
+            // btnAdminTraditions
+            // 
+            this.btnAdminTraditions.AutoSize = true;
+            this.btnAdminTraditions.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAdminTraditions.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdminTraditions.Location = new System.Drawing.Point(10, 151);
+            this.btnAdminTraditions.Margin = new System.Windows.Forms.Padding(10);
+            this.btnAdminTraditions.Name = "btnAdminTraditions";
+            this.btnAdminTraditions.Size = new System.Drawing.Size(176, 63);
+            this.btnAdminTraditions.TabIndex = 1;
+            this.btnAdminTraditions.Text = "Fiestas y tradiciones";
+            this.btnAdminTraditions.UseVisualStyleBackColor = true;
+            this.btnAdminTraditions.Click += new System.EventHandler(this.btnAdminTraditions_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.AutoSize = true;
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(10, 483);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(10);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(176, 68);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Salir";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnAdminUsers
+            // 
+            this.btnAdminUsers.AutoSize = true;
+            this.btnAdminUsers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAdminUsers.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdminUsers.Location = new System.Drawing.Point(10, 400);
+            this.btnAdminUsers.Margin = new System.Windows.Forms.Padding(10);
+            this.btnAdminUsers.Name = "btnAdminUsers";
+            this.btnAdminUsers.Size = new System.Drawing.Size(176, 63);
+            this.btnAdminUsers.TabIndex = 6;
+            this.btnAdminUsers.Text = "Usuarios";
+            this.btnAdminUsers.UseVisualStyleBackColor = true;
+            this.btnAdminUsers.Click += new System.EventHandler(this.btnAdminUsers_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form3";
+            this.Text = "Administrador";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnAdminPlace;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnAdminRoutes;
+        private System.Windows.Forms.Button btnAdminRecipes;
+        private System.Windows.Forms.Button btnAdminTraditions;
+        private System.Windows.Forms.Button btnAdminUsers;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
