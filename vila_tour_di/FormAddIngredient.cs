c57ec1e5006c7bb567dd2fc9aa0686fc4a0f39fc
Omar Llinares <150013139+LlinaresOmar@users.noreply.github.com>
2024-11-10 19:54:48 +0100

    conexion db

--- vila_tour_di/FormAddIngredient.cs
+++ vila_tour_di/FormAddIngredient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClientRESTAPI;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,30 +17,55 @@ namespace vila_tour_di {
             // Limpiar el campo de nombre
             textBoxNameIng.Text = string.Empty;
 
-            // Seleccionar el primer tipo de ingrediente en el ComboBox
-            //comboBoxTypeIng.SelectedIndex = 0;
+            // Asigna los valores del enum al ComboBox
+            comboBoxTypeIng.DataSource = Enum.GetValues(typeof(IngredientType));
         }
 
         public FormAddIngredient(string name, string type) {
             InitializeComponent();
 
-            // Asignar el nombre del ingrediente
-            textBoxNameIng.Text = name;
-
-            // Buscar el índice del tipo de ingrediente en el ComboBox
-            int index = comboBoxTypeIng.Items.IndexOf(type);
-
-            // Si el tipo existe en el ComboBox, seleccionarlo; si no, dejar el primer valor
-            comboBoxTypeIng.SelectedIndex = (index >= 0) ? index : 0;
+            comboBoxTypeIng.DataSource = Enum.GetValues(typeof(IngredientType));
         }
 
         private void buttonExit_Click(object sender, EventArgs e) {
             Dispose();
         }
 
-        private void buttonAddIng_Click(object sender, EventArgs e)
-        {
+        // Logica para añadir ingreediente
+        private void buttonAddIng_Click(object sender, EventArgs e){
+            /*
+             * Aqui se obtienen los datos y se genera el JSON que se envia
+             */
 
+            String nameIngredient = textBoxNameIng.Text;
+            String category = comboBoxTypeIng.Text;
+
+            String url = "http://127.0.0.1:8080/ingredients";
+
+            RestClient r = new RestClient(url, "POST");
+
+            String datos = "{" +
+                "\"name\": \"" + nameIngredient + "\", " +
+                "\"category\": \"" + category + "\"" +
+                "}";
+
+            String res = r.postItem(datos);
+
+            MessageBox.Show("Resultado: " + res);
+        }
+
+        public enum IngredientType {
+            FRUITS_AND_VEGETABLES,
+            MEAT_AND_POULTRY,
+            FISH_AND_SEAFOOD,
+            DAIRY,
+            GRAINS_AND_CEREALS,
+            SPICES_AND_HERBS,
+            LEGUMES,
+            NUTS_AND_SEEDS,
+            OILS_AND_FATS,
+            SWEETENERS,
+            BEVERAGES
         }
     }
 }
---
+++ vila_tour_di/RestClient.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientRESTAPI {
+    internal class RestClient {
+        private String url; // url de nuestra base de datos
+        private HttpWebRequest request;
+
+        public RestClient(String url, String tipo) {
+            /*
+             * Constructor que inicializa las peticiones web, requiere url y GET o POST (o PUT O DELETE)
+             * 
+             */
+
+            this.url = url;
+            this.request = (HttpWebRequest)WebRequest.Create(this.url);
+            this.request.Method = "GET";
+
+            switch (tipo) {
+                case "GET":
+                    this.request.Method = "GET";
+                    break;
+                case "POST":
+                    this.request.Method = "POST";
+                    break;
+            }
+
+            this.request.ContentType = "application/json";
+            request.Accept = "application/json";
+
+
+        }
+
+
+        public String getItem() {
+            /* Método que hace una peticion get de vuelos por origen
+             * 
+             */
+            try {
+                WebResponse response = request.GetResponse();
+                Stream strReader = response.GetResponseStream();
+
+                if (strReader != null) {
+
+                    StreamReader sr = new StreamReader(strReader);
+                    string str = sr.ReadToEnd();
+                    return str;
+                } else return null;
+            } catch (WebException e) {
+
+                return null;
+            }
+
+        }
+
+
+
+
+        public String postItem(String data) {
+
+            /*
+             El string data que le pasamos es un json válido de petición post (añadir por ejemplo)
+             */
+
+            try {
+                Stream strWriter = request.GetRequestStream();
+                if (strWriter != null) {
+                    StreamWriter sw = new StreamWriter(strWriter);
+
+                    sw.Write(data);
+                    sw.Flush();
+                    sw.Close();
+
+                    // Hasta aqui hemos hecho la petición POST
+                    // Ahora obtenemos la respuesta del server
+
+                    WebResponse response = request.GetResponse();
+                    Stream strReader = response.GetResponseStream();
+                    if (strReader != null) {
+                        StreamReader sr = new StreamReader(strReader);
+                        String respuesta = sr.ReadToEnd();
+                        return respuesta;
+                    }
+
+                } else return null;
+            } catch (WebException e) {
+
+                return null;
+            }
+
+            return null;
+        }
+
+
+
+    }
+}
